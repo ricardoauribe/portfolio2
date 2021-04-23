d09b2bdaf182aff432baeeff72a7de5eec291629
Ricardo <ricardoauribe@hotmail.com>
2021-04-23 18:39:59 -0500

    Changing NavBar to pure bootstrap

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,55 @@
 <template>
   <header>
-    <nav class="navbar fixed-top">
-      <div class="flex-grow-1 text-left">
-        <div class="title">Ricardo Uribe</div>
-      </div>
-    </nav>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item dropdown dmenu">
+            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+              Our Service
+            </a>
+            <div class="dropdown-menu sm-menu">
+              <a class="dropdown-item" href="#">service2</a>
+              <a class="dropdown-item" href="#">service 2</a>
+              <a class="dropdown-item" href="#">service 3</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Call</a>
+          </li>
+           <!-- <li class="nav-item dropdown dmenu">
+            <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+              Dropdown link
+            </a>
+            <div class="dropdown-menu sm-menu">
+              <a class="dropdown-item" href="#">Link 1</a>
+              <a class="dropdown-item" href="#">Link 2</a>
+              <a class="dropdown-item" href="#">Link 3</a>
+              <a class="dropdown-item" href="#">Link 4</a>
+              <a class="dropdown-item" href="#">Link 5</a>
+              <a class="dropdown-item" href="#">Link 6</a>
+            </div>
+          </li> -->
+          </ul>
+          <div class="social-part">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+          </div>
+        </div>
+      </nav>
   </header>
 </template>
 
@@ -15,21 +60,29 @@ export default {
 </script>
 
 <style>
-    .navbar {
-      background-color: #000;
-      padding-left: 15px;
-      padding-top: 8px;
-      padding-right: 15px;
-      padding-bottom: 4px;
-      margin-top: 0px;
-      align-items: right;
-    }
-    .title {
-        color: #C6EFFF;
-        font-weight: 600;
-        font-size: larger;
-        margin: 0px;
-        padding-bottom: 0px;
-        margin-bottom: 0px;
-    }
+  .navbar {
+    background-color: #000;
+    padding-left: 15px;
+    padding-top: 8px;
+    padding-right: 15px;
+    padding-bottom: 4px;
+    margin-top: 0px;
+    align-items: right;
+  }
+  .title {
+      color: #C6EFFF;
+      font-weight: 600;
+      font-size: larger;
+      margin: 0px;
+      padding-bottom: 0px;
+      margin-bottom: 0px;
+  }
+
+  .social-part .fa{
+    padding-right:20px;
+  }
+  ul li a{
+      margin-right: 20px;
+  }
+
 </style>
